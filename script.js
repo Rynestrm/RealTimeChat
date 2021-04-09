621d4f434a3b832b52bdc5f03d9d68c177a59087
Ivan Gibson <gibson.ivan@gmail.com>
2021-04-08 20:11:55 -0400

    Changes to names.

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
+//client side scripts
+
+// getting the socket variable and the location of the host
 const socket = io('http://localhost:3000');
-const messageForm = document.getElementById('sendContainer');
-const messageInput = document.getElementById('messageInput');
-const messageContainer = document.getElementById('messageContainer')
+const messageForm = document.querySelector('form');
+const messageInput = document.querySelector('input');
+const container = document.getElementById('container');
 
-const name = prompt("What is your name?");
+// asks a new user their name
+const name = prompt("Please enter your name");
 
-appendMessage('You joined');
-socket.emit('newUser', name);
+displayMessage('You have joined the chat!');
+// when a user joins it will send the name variable the user just entered
+socket.emit('user', name);
 
-socket.on('chatMessage', data => {
-   appendMessage(`${data.name}: ${data.message}`);
+// use socket.on for an event called 'chatMessage
+// every time an event is received the data is sent from the server 
+socket.on('chatMessage', function(data){
+   // shows the name and message of anything sent to the server
+   displayMessage(`${data.name}: ${data.message}`);
 })
 
-socket.on('userConnected', name => {
-   appendMessage(`${name} Connected`);
-})
-
-socket.on('userDisconnected', name => {
-   appendMessage(`${name} Disconnected`);
-})
-
-messageForm.addEventListener('submit', e => {
+//stops the form from posting and refreshing
+messageForm.addEventListener('submit', function(e){
    e.preventDefault();
+   //getting the actual message from the input
    const message = messageInput.value;
-   appendMessage(`You: ${message}`);
-   socket.emit('sendChatMessage', message);
+   // puts 'you' in front of all message sent by you
+   displayMessage(`You: ${message}`);
+   // sending event called "sendMessage" from client to server
+   socket.emit('sendMessage', message);
+   // clears the form every time it is sent
    messageInput.value = '';
 })
 
-function appendMessage(message){
-   const MessageElement = document.createElement('div');
-   MessageElement.innerText = message;
-   messageContainer.append(MessageElement);
+// every time a user connects it is displayed to all other users
+socket.on('userConnected', function(name){
+   // puts the users name into the connected message
+   displayMessage(`${name} Connected`);
+})
+
+//out put the messages to the page.
+function displayMessage(message){
+   // making a new div for each element
+   const MessageDiv = document.createElement('div');
+   //this takes the div and puts the message into it
+   MessageDiv.innerHTML = message;
+   //appending the message to the container
+   container.append(MessageDiv);
 }
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
+//server side scripts
+
+// use socket io and pass in 3000 for the port
 const io = require('socket.io')(3000, {
+// used to stop CORS
    cors: {
       origin: "*"
    }
 });
-
+// used to store user names
 const users = {}
-
-io.on('connection', socket =>{
-   socket.on('newUser', name =>{
+// every time a user log on it runs this function and assigns a socket to the user.
+io.on('connection', function(socket){
+   // takes the name the user entered on the client side and passes in the name
+   socket.on('user', function(name){
+      // every user with a unique socket id will have their name assigned to it
       users[socket.id] = name
+      //broadcasts the name of the user that connected.
       socket.broadcast.emit('userConnected', name)
    })
-   socket.on('sendChatMessage', message =>{
+   socket.on('sendMessage', function(message){
+      // broadcasts 'chatMessage' to every single person except the person who sent the message and puts the sending users name on it.
       socket.broadcast.emit('chatMessage', {message: message, name: users[socket.id]})
    })
-   socket.on('disconnected', () =>{
-      socket.broadcast.emit('userDisconnected', users[socket.id])
-      delete users[socket.id]
-   })
+   // socket.on('disconnected', function(){
+   //    socket.broadcast.emit('userDisconnected', users[socket.id])
+   //    delete users[socket.id]
+   // })
 });
