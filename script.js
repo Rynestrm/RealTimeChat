01dd5a1e2fbc013928950e525006712085c6183f
Ivan Gibson <gibson.ivan@gmail.com>
2021-04-13 15:52:24 -0400

    Changed host location for testing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //client side scripts
 
 // getting the socket variable and the location of the host
-const socket = io('http://localhost:3000');
+const socket = io('https://rynestrm.github.io/RealTimeChat/');
 const messageForm = document.querySelector('form');
 const messageInput = document.querySelector('input');
 const container = document.getElementById('container');
@@ -9,20 +9,23 @@ const container = document.getElementById('container');
 // asks a new user their name
 const name = prompt("Please enter your name");
 
+// displays a message to the user they have joined the chat
 displayMessage('You have joined the chat!');
+
 // when a user joins it will send the name variable the user just entered
 socket.emit('user', name);
 
 // use socket.on for an event called 'chatMessage
 // every time an event is received the data is sent from the server 
+
 socket.on('chatMessage', function(data){
    // shows the name and message of anything sent to the server
    displayMessage(`${data.name}: ${data.message}`);
 })
 
 //stops the form from posting and refreshing
-messageForm.addEventListener('submit', function(e){
-   e.preventDefault();
+messageForm.addEventListener('submit', function(evt){
+   evt.preventDefault();
    //getting the actual message from the input
    const message = messageInput.value;
    // puts 'you' in front of all message sent by you
@@ -33,18 +36,12 @@ messageForm.addEventListener('submit', function(e){
    messageInput.value = '';
 })
 
-// every time a user connects it is displayed to all other users
-socket.on('userConnected', function(name){
-   // puts the users name into the connected message
-   displayMessage(`${name} Connected`);
-})
-
-//out put the messages to the page.
+//output the messages to the page.
 function displayMessage(message){
    // making a new div for each element
    const MessageDiv = document.createElement('div');
    //this takes the div and puts the message into it
    MessageDiv.innerHTML = message;
-   //appending the message to the container
+   //appending the message to the container div
    container.append(MessageDiv);
 }
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 //server side scripts
 
-// use socket io and pass in 3000 for the port
+// use socket io on the server and pass in 3000 for the port
 const io = require('socket.io')(3000, {
-// used to stop CORS
+// used to stop CORS errors in browser.
    cors: {
       origin: "*"
    }
 });
+
 // used to store user names
 const users = {}
-// every time a user log on it runs this function and assigns a socket to the user.
+
+// every time the pages is loaded it runs this function and assigns a socket id to the user with the event name of 'connection'
 io.on('connection', function(socket){
    // takes the name the user entered on the client side and passes in the name
    socket.on('user', function(name){
       // every user with a unique socket id will have their name assigned to it
       users[socket.id] = name
       //broadcasts the name of the user that connected.
-      socket.broadcast.emit('userConnected', name)
+      // socket.broadcast.emit('userConnected', name)
    })
+   
    socket.on('sendMessage', function(message){
-      // broadcasts 'chatMessage' to every single person except the person who sent the message and puts the sending users name on it.
+      // broadcasts 'chatMessage' event to every single person except the person who sent the message and puts the sending users name on it.
       socket.broadcast.emit('chatMessage', {message: message, name: users[socket.id]})
    })
 });
